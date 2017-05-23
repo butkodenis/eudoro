b79a91e5dbd05c4b3a1d33d7a6c6533b1526e1ed
butkodenis <Butko.dv@gmail.com>
2017-05-23 15:41:12 +0300

    anim

--- js/main.js
+++ js/main.js
@@ -1,7 +1,60 @@
-// jQuery(document).ready(function() {
-//     new WOW().init();
-//     jQuery('section').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animated fadeIn',
-//         offset: 100
-//     });
-// });
+    //
+
+    $.noConflict();
+
+    jQuery(document).ready(function ($) {
+
+        /// анимация
+        new WOW().init();
+
+
+
+        // плавная прокрутка
+        $('a[href^="#"]').click(function(){
+            var target = $(this).attr('href');
+            $('html, body').animate({scrollTop: $(target).offset().top}, 1500);
+            return false;
+        });
+
+
+
+
+        // фильтрация блоков портфолио  ( незабыть .mix { display: none;})
+
+        $(function(){
+            $("#filter-block").mixItUp({
+
+            });
+        });
+
+        //   show more
+
+        // size_li = $("#filter-block > div").size();
+        // x=9;
+        // console.log(size_li);
+        // $('#filter-block  > div:lt('+x+')').show();
+        // $('#loadMore').click(function () {
+        //     x = (x+3 <= size_li) ? x+3 : size_li;
+        //     $('#filter-block > div:lt('+x+')').show();
+        // });
+
+
+
+        // изминение при прокрутке nav
+        if ($('#service').length > 0) {
+            (function () {
+                $(window).on("scroll", function() {
+                    if ($(window).scrollTop() > 50) $('.navbar').addClass('sticky');
+                    else $('.navbar').removeClass('sticky');
+                });
+            })();
+        }
+
+
+
+    });
+
+
+
+
+
