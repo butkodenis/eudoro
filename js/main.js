089699142f2c5d11d1915c2f6b4f23b2e8479aba
butkodenis <Butko.dv@gmail.com>
2017-05-24 12:58:07 +0300

    finm

--- js/main.js
+++ js/main.js
@@ -15,17 +15,32 @@
             $('html, body').animate({scrollTop: $(target).offset().top}, 1500);
             return false;
         });
+        // фильтр
+        $('#filtr-2').click(function () {
+            $('.food-menu-block img').attr("src",'images/item1.png');
+        });
+        $('#filtr-3').click(function () {
+            $('.food-menu-block img').attr("src",'images/item4.png');
+        });
+        $('#filtr-1').click(function () {
+            $('#img-menu-1').attr("src",'images/item1.png');
+            $('#img-menu-2').attr("src",'images/item2.png');
+            $('#img-menu-3').attr("src",'images/item3.png');
+            $('#img-menu-4').attr("src",'images/item4.png');
+            $('#img-menu-5').attr("src",'images/item3.png');
 
+
+        });
 
 
 
         // фильтрация блоков портфолио  ( незабыть .mix { display: none;})
-
-        $(function(){
-            $("#filter-block").mixItUp({
-
-            });
-        });
+        //
+        // $(function(){
+        //     $("#filter-block").mixItUp({
+        //
+        //     });
+        // });
 
         //   show more
 
@@ -44,8 +59,8 @@
         if ($('#service').length > 0) {
             (function () {
                 $(window).on("scroll", function() {
-                    if ($(window).scrollTop() > 50) $('.navbar').addClass('sticky');
-                    else $('.navbar').removeClass('sticky');
+                    if ($(window).scrollTop() > 50) $('.navbar').addClass('sticky') && $('#logo').addClass('.logo_sm');
+                    else $('.navbar').removeClass('sticky')&& $('#logo').removeClass('.logo_sm');
                 });
             })();
         }
